e43a393832425e89a0912776758a947c3d552e38
Henrique Recidive <recidive@gmail.com>
2013-08-06 18:01:03 -0300

    Adding some basic mocha tests for Prana core functionality and models.

---
+++ test/model.js
@@ -0,0 +1,101 @@
+var assert = require("assert");
+var Prana = require('../prana');
+
+describe('Model', function() {
+
+  var prana = new Prana();
+
+  var SomeType = prana.type(new Prana.Type('someType', {
+    title: 'Some Type',
+    description: 'Some type for testing purposes.'
+  }));
+
+  it('should save and load an item', function(done) {
+    var item = new SomeType({
+      key: 1,
+      value: 'Some value'
+    });
+
+    item.save(function(err) {
+      if (err) {
+        throw err;
+      }
+
+      assert.ok(item);
+
+      SomeType.load(1, function(err, item) {
+        if (err) {
+          throw err;
+        }
+        assert.ok(item);
+        assert.ok(item instanceof SomeType);
+        assert.ok(item instanceof Prana.Model);
+        done();
+      });
+    });
+  });
+
+  it('should save and list items', function(done) {
+    var item = new SomeType({
+      key: 2,
+      value: 'Some value'
+    });
+
+    item.save(function(err) {
+      if (err) {
+        throw err;
+      }
+
+      assert.ok(item);
+
+      SomeType.list({}, function(err, items) {
+        if (err) {
+          throw err;
+        }
+        assert.ok(items);
+        assert.ok(Object.keys(items).length > 0);
+        done();
+      });
+    });
+  });
+
+  it('should save, load and delete an item', function(done) {
+    var item = new SomeType({
+      key: 3,
+      value: 'Some value'
+    });
+
+    item.save(function(err) {
+      if (err) {
+        throw err;
+      }
+
+      assert.ok(item);
+
+      SomeType.load(3, function(err, item) {
+        if (err) {
+          throw err;
+        }
+        assert.ok(item);
+        assert.ok(item instanceof SomeType);
+        assert.ok(item instanceof Prana.Model);
+
+        item.delete(function(err, deletedItem) {
+          if (err) {
+            throw err;
+          }
+          assert.ok(deletedItem);
+
+          SomeType.load(3, function(err, absentItem) {
+            if (err) {
+              throw err;
+            }
+            assert.ok(!absentItem);
+            done();
+          });
+        });
+      });
+    });
+  });
+
+});
---
+++ test/prana.js
@@ -0,0 +1,58 @@
+var assert = require("assert");
+var Prana = require('../prana');
+
+describe('Prana', function() {
+
+  it('should create a model for a type', function(done) {
+    var prana = new Prana();
+
+    var SomeType = prana.type(new Prana.Type('someType', {
+      title: 'Some Type',
+      description: 'Some type for testing purposes.'
+    }));
+
+    var someItem = new SomeType({
+      key: 1,
+      value: 'Some value'
+    });
+    assert.ok(someItem instanceof Prana.Model);
+
+    var SomeTypeAgain = prana.type('someType');
+    assert.equal(SomeTypeAgain, SomeType);
+
+    var Type = prana.type('type');
+
+    Type.load('someType', function(err, item) {
+      if (err) {
+        throw err;
+      }
+      assert.ok(item);
+      done();
+    });
+  });
+
+  it('should store a extension', function(done) {
+    var prana = new Prana();
+
+    var someExtension = prana.extension(new Prana.Extension(prana, 'some-extension', {}, {
+      title: 'Some Extension',
+      description: 'Some extension for testing purposes.'
+    }));
+    assert.ok(someExtension instanceof Prana.Extension);
+
+    var someExtensionAgain = prana.extension('some-extension');
+    assert.equal(someExtensionAgain, someExtension, 'Both extensions are the same.');
+
+    var Extension = prana.type('extension');
+
+    Extension.load('some-extension', function(err, item) {
+      if (err) {
+        throw err;
+      }
+      assert.ok(item);
+      assert.ok(item instanceof Prana.Extension);
+      done();
+    });
+  });
+
+});
